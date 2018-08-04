f8ce84d2e7247f4cfba08b0340b0ff0e98e9887a
hejianfang <1343574826@qq.com>
2018-08-04 11:35:40 +0800

    最终版

--- controller/upload.js
+++ controller/upload.js
@@ -2,66 +2,29 @@ const {Router} = require("express");
 const router = Router();
 const mime = require('mime');
 const multer = require('multer');
-const accept = [
-    mime.getType('jpg'),
-    mime.getType('png'),
-    mime.getType('gif'),
-    mime.getType('webp')
-];
+var path = require('path');
 
-const storage = multer.diskStorage({
+var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './uploads')
+        cb(null, path.resolve(__dirname,'../uploads'))
     },
     filename: function (req, file, cb) {
-        let extname = mime.getExtension(file.mimetype);
-        cb(null, file.fieldname + '-' + Date.now() + '.' +extname)
+        console.log(file)
+        let extname = mime.getExtension(file.mimetype)
+        cb(null, file.fieldname + '-' + Date.now() +'.'+extname)
     }
 })
 
-function fileFilter (req, file, cb) {
-    let isAccept = false;
-    accept.forEach(item => {
-        if(item == file.mimetype){
-            isAccept = true;
-        }
-    })
-    if(isAccept){
-        cb(null, true)
-    }
-    else {
-        console.log(11);
-        cb(new Error(`don't accept mimeType`))
-    }
-}
-const upload = multer({
-    storage,
-    limits: {
-        fileSize: 1024*1024*2
-    },
-    fileFilter,
-    preservePath: true
+var upload = multer({
+    storage
 })
-const middleFun = upload.single('pic')
-
-router.post('/upload',(req,res) => {
-    middleFun(req, res, function (err) {
-        if(err){
-            console.log(22);
-            res.json({
-                code: 401,
-                msg: '文件大小超出限制或上传文件类型不合法'
-            })
-            return
-        }
+router.post('/upload', upload.single('pic'),
+    (req, res) => {
+        let filePath = req.file.filename
         res.json({
-            code: 200,
-            data: {
-                path: req.file.path.replace('uploads\\','/'),
-            }
+            code:200,
+            data: filePath
         })
     })
-
-})
 
 module.exports = router;
